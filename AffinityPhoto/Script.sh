76b8bf8b699d25555f4e553f747761376fe4720b
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-09-05 17:47:35 +0300

    Update Script.sh

--- AffinityPhoto/Script.sh
+++ AffinityPhoto/Script.sh
@@ -64,9 +64,6 @@ rm "/home/$USER/.local/share/applications/wine/Programs/Affinity Photo 2.desktop
 # Create Desktop Entry
 desktop_entry="$HOME/.local/share/applications/AffinityPhoto.desktop"
 
-
-StartupWMClass=photo.exe
-
 #Desktop file stuff
 echo "[Desktop Entry]" >> ~/.local/share/applications/AffinityPhoto.desktop
 echo "Name=Affinity Photo" >> ~/.local/share/applications/AffinityPhoto.desktop
