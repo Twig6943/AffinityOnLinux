3bc97b252e62c22f99bf4a7fac9f7eccff3fc708
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2024-09-07 13:28:17 +0300

    Update and rename asda to Script.sh

---
+++ AffinityDesigner/Script.sh
@@ -0,0 +1,108 @@
+p#!/bin/bash
+# This script installs Affinity Photo using WINE. Ensure you have WINE,WINETRICKS WGET, 7Z, and TAR installed.
+
+# Check for required dependencies
+missing_deps=""
+
+check_dependency() {
+  if ! command -v "$1" &> /dev/null; then
+    missing_deps+="$1 "
+  fi
+}
+
+check_dependency "wine"
+check_dependency "winetricks"
+check_dependency "wget"
+check_dependency "curl"
+check_dependency "7z"
+check_dependency "tar"
+
+if [ -n "$missing_deps" ]; then
+  echo "The following dependencies are missing: $missing_deps"
+  echo "Please install them and rerun the script."
+  exit 1
+fi
+
+echo "All dependencies are installed!"
+sleep 2
+
+directory="$HOME/.AffinityLinux"
+
+#Kill wine
+wineserver -k
+# Create install directory
+mkdir -p "$directory"
+
+#Download latest wine 
+latest_release_url=$(curl -s https://api.github.com/repos/Twig6943/ElementalWarrior-Wine-binaries/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+
+# Download the latest wine release
+wget "$latest_release_url" -O "$directory/ElementalWarriorWine.tar.gz"
+
+# Download files
+wget https://upload.wikimedia.org/wikipedia/commons/3/3c/Affinity_Designer_2-logo.svg -O "/home/$USER/.local/share/icons/AffinityDesigner.svg"
+wget https://archive.org/download/win-metadata/WinMetadata.zip -O "$directory/Winmetadata.zip"
+
+# Extract wine binary
+tar -xvzf "$directory/ElementalWarriorWine.tar.gz" -C "$directory"
+
+# Erase the ElementalWarriorWine.tar.gz
+rm "$directory/ElementalWarriorWine.tar.gz"
+
+# WINETRICKS stuff
+WINEPREFIX="$directory" winetricks --unattended dotnet48 corefonts
+WINEPREFIX="$directory" winetricks renderer=vulkan
+
+# Extract & delete WinMetadata.zip
+7z x "$directory/Winmetadata.zip" -o"$directory/drive_c/windows/system32"
+rm "$directory/Winmetadata.zip"
+# Start the setup
+echo "Put Affinity Photo's setup .exe to $directory and press any key when ready."
+read -n 1
+
+echo "Click No if you get any errors. Press any key to continue."
+read -n 1
+
+#Set windows version to 11
+WINEPREFIX="$directory" "$directory/ElementalWarriorWine/bin/winecfg" -v win11
+WINEPREFIX="$directory" "$directory/ElementalWarriorWine/bin/wine" "$directory"/*.exe
+rm "$directory"/affinity*.exe
+
+#Wine dark theme
+wget https://raw.githubusercontent.com/Twig6943/AffinityOnLinux/main/wine-dark-theme.reg -O "$directory/wine-dark-theme.reg"
+WINEPREFIX="$directory" "$directory/ElementalWarriorWine/bin/regedit" "$directory/wine-dark-theme.reg"
+rm "$directory/wine-dark-theme.reg"
+
+#Remove Desktop entry created by wine
+rm "/home/$USER/.local/share/applications/wine/Programs/Affinity Designer 2.desktop"
+
+# Create Desktop Entry
+echo "[Desktop Entry]" >> ~/.local/share/applications/AffinityDesigner.desktop
+echo "Name=Affinity Photo" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Comment=Affinity Designer is a graphic designing and UX solution that helps businesses create concept art, logos, icons, UI designs, print projects and mock-ups, among other illustrations." >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Icon=/home/$USER/.local/share/icons/AffinityDesigner.svg" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Path=$directory" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Exec=env WINEPREFIX=$directory $directory/ElementalWarriorWine/bin/wine \"$directory/drive_c/Program Files/Affinity/Designer 2/Designer.exe\"" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Terminal=false" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "NoDisplay=false" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "StartupWMClass=designer.exe" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Type=Application" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "Categories=Graphics;" >> ~/.local/share/applications/AffinityPhoto.desktop
+echo "StartupNotify=true" >> ~/.local/share/applications/AffinityPhoto.desktop
+
+cp ~/.local/share/applications/AffinityPhoto.desktop ~/Desktop/AffinityPhoto.desktop
+
+# Copy to desktop
+cp "$HOME/.local/share/applications/AffinityPhoto.desktop" "$HOME/Desktop/AffinityPhoto.desktop"
+
+# Special Thanks section
+echo "******************************"
+echo "    Special Thanks"
+echo "******************************"
+echo "Ardishco (github.com/raidenovich)"
+echo "Deviaze"
+echo "Kemal"
+echo "Jacazimbo <3"
+echo "Kharoon"
+echo "Jediclank134"
+read -n 1
